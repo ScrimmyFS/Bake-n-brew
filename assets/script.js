b1b2fb54dd635480999fc81a4068f475c04d1f96
ScrimmyFS <dscrimshaw12345@gmail.com>
2022-12-19 21:34:37 -0500

    added some functionality to the submit button

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,18 @@ var listEL = $("#ingredient-list");
 var submitInput = $("#ingredientInput");
 var addIngredient = $("#sbmit");
 var erase = $("#clear");
+var submitbutton = $("#submitbutton");
+var apikey = "d118999455904a82adf6e360ee3fa28e";
+var cardcontainer = $('#cardcontainer');
 console.log(submitInput);
 
 function submissionForm(event) {
   event.preventDefault();
   var ingredientItem = submitInput.val();
   listEL.append("<li>" + ingredientItem + "<li>");
+  saveIngredients()
 
 
-  
 }
 
 function clearIngredients(event) {
@@ -34,10 +37,61 @@ function saveIngredients() {
   localStorage.setItem("ingredient", JSON.stringify(storedIngredients));
 }
 
-function clearLocalStorage() {
+function clearLocalStorage(storedIngredients) {
   storedIngredients = [];
   localStorage.setItem("ingredient", JSON.stringify(storedIngredients));
 }
 
+function Searchbutton(event) {
+  event.preventDefault()
+
+ var storedIngredients = JSON.parse(localStorage.getItem("ingredient"));
+ console.log(storedIngredients)
+
+  var api = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + storedIngredients +  "&number=4&limit=3&apiKey=" + apikey;
+
+
+
+fetch(api, {
+      method: "GET",
+      credntials: 'same-orgin',
+      redirect: 'follow',
+    })
+    
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data){
+      console.log(data);
+    
+
+    for (var i = 0; i = data.length; i++){
+      cardcontainer.innerhtml +=`
+      <div class="col s12 m6 l3">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="${data[i].image}">
+                            <span class="card-title"></span>
+                        </div>
+                        <div class="card-content">
+                        </div>
+                    </div>
+
+                </div>
+      `
+
+      // var picture = document.createElement('<img>')
+
+      // picture.
+
+
+
+
+
+    }
+  })
+
+}
+submitbutton.on("click", Searchbutton)
 addIngredient.on("click", submissionForm);
 erase.on("click", clearIngredients);
